2cbd46994910a68651480635fe04aac6c8ef3547
Ignacio Burgueño <ignaciob@inconcertcc.com>
2011-05-30 09:46:49 -0300

    Removed 'defined but not used' warnings on gcc

--- lcbTypeChecks.h
+++ lcbTypeChecks.h
@@ -7,8 +7,15 @@
 #include "lcbException.h"
 #include <string.h>
 
+#ifdef __GNUC__
+# define UNUSED __attribute__((unused))
+#else
+# define UNUSED
+#endif
+
 namespace LuaCppBridge
 {
+	UNUSED
 	static int error (lua_State *L, const char *fmt, ...) {
 		va_list argp;
 		va_start(argp, fmt);
@@ -19,6 +26,7 @@ namespace LuaCppBridge
 		throw Type_error(L, true); //return lua_error(L);
 	}
 
+	UNUSED
 	static int argerror (lua_State *L, int narg, const char *extramsg) {
 		lua_Debug ar;
 		if (!lua_getstack(L, 0, &ar))  /* no stack frame? */
@@ -36,6 +44,7 @@ namespace LuaCppBridge
 			narg, ar.name, extramsg);
 	}
 
+	UNUSED
 	static int typerror (lua_State *L, int narg, const char *tname) {
 		const char* msg = lua_pushfstring(L, "%s expected, got %s", tname, 
 			luaL_typename(L, narg));
@@ -43,18 +52,21 @@ namespace LuaCppBridge
 	}
 
 	namespace detail {
+		UNUSED
 		static void tag_error (lua_State *L, int narg, int tag) {
 			typerror(L, narg, lua_typename(L, tag));
 		}
 		
 	}
 	
+	UNUSED
 	static const char* checklstring (lua_State *L, int narg, size_t *len) {
 		const char *s = lua_tolstring(L, narg, len);
 		if (!s) detail::tag_error(L, narg, LUA_TSTRING);
 		return s;
 	}
 	
+	UNUSED
 	static lua_Number checknumber (lua_State *L, int narg) {
 		lua_Number d = lua_tonumber(L, narg);
 		if (d == 0 && !lua_isnumber(L, narg))  /* avoid extra test when d is not 0 */
@@ -62,6 +74,7 @@ namespace LuaCppBridge
 		return d;
 	}
 	
+	UNUSED
 	static lua_Integer checkinteger (lua_State *L, int narg) {
 		lua_Integer d = lua_tointeger(L, narg);
 		if (d == 0 && !lua_isnumber(L, narg))  /* avoid extra test when d is not 0 */
@@ -69,26 +82,30 @@ namespace LuaCppBridge
 		return d;
 	}
 	
-	static lua_Integer optinteger (lua_State *L, int narg,
-														lua_Integer def) {
+	UNUSED
+	static lua_Integer optinteger (lua_State *L, int narg, lua_Integer def) {
 		return luaL_opt(L, checkinteger, narg, def);
 	}
 	
+	UNUSED
 	static void checkstack (lua_State *L, int sz, const char *msg) {
 		if (!lua_checkstack(L, sz))
 			error(L, "stack overflow (%s)", msg);
 	}
 	
+	UNUSED
 	static void checktype (lua_State *L, int narg, int t) {
 		if (lua_type(L, narg) != t)
 			detail::tag_error(L, narg, t);
 	}
 	
+	UNUSED
 	static void checkany (lua_State *L, int narg) {
 		if (lua_type(L, narg) == LUA_TNONE)
 			argerror(L, narg, "value expected");
 	}
 	
+	UNUSED
 	static void *luaL_checkudata (lua_State *L, int ud, const char *tname) {
 		void *p = lua_touserdata(L, ud);
 		if (p != NULL) {  /* value is a userdata? */
@@ -104,8 +121,8 @@ namespace LuaCppBridge
 		return NULL;  /* to avoid warnings */
 	}
 	
-	static const char* optlstring (lua_State *L, int narg,
-                                        const char *def, size_t *len) {
+	UNUSED
+	static const char* optlstring (lua_State *L, int narg, const char *def, size_t *len) {
 		if (lua_isnoneornil(L, narg)) {
 			if (len)
 				*len = (def ? strlen(def) : 0);
@@ -114,32 +131,38 @@ namespace LuaCppBridge
 		else return checklstring(L, narg, len);
 	}
 
+	UNUSED
 	static const char* checkstring (lua_State *L, int narg) {
 		return checklstring(L, narg, NULL);
 	}
 	
+	UNUSED
 	static const char* optstring (lua_State *L, int narg,
                                         const char *def) {
 		return optlstring(L, narg, def, NULL);
 	}
 	
+	UNUSED
 	static lua_Integer checkint (lua_State *L, int narg) {
 		return checkinteger(L, narg);
 	}
 	
+	UNUSED
 	static lua_Integer checkint (lua_State *L, int narg, lua_Integer def) {
 		return optinteger(L, narg, def);
 	}
 	
+	UNUSED
 	static lua_Integer checklong (lua_State *L, int narg) {
 		return checkinteger(L, narg);
 	}
 	
+	UNUSED
 	static lua_Integer optlong (lua_State *L, int narg, lua_Integer def) {
 		return optinteger(L, narg, def);
 	}
 	
-
+	UNUSED
 	static int checkoption (lua_State *L, int narg, const char *def,
 								const char *const lst[]) {
 		const char *name = (def) ? optstring(L, narg, def) : checkstring(L, narg);
